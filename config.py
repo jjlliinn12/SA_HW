d48ec634141b42b0b43a86ba9a31c4fdb2cd7484
jjlliinn12 <jasonclin531@gmail.com>
2017-12-02 20:12:28 -0600

    Code and news tweets

---
+++ config.py
@@ -0,0 +1,4 @@
+consumer_key = "8z9OHiITNpz3rDpzqKWkmNOqO"
+consumer_secret = "qyodEDqRfWHTzisEHjFaDmZO8yW4thztax5Z32LYHjoKOcDTWi"
+access_token = "776605862815072257-QN0Dn3nLQzUhil9CF5qwZyNBkHVGBWR"
+access_token_secret = "B8WU5wKX6y7xd6yfasplQ7sdqTImvzL2a1fC7lDjZB6HQ"
